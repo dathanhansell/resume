0be9a0e87f90008d741e9d94f28671069225f57e
dathanhansell <dathanhansell@gmail.com>
2023-11-24 22:56:12 -0800

    added modification support

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # resume
-A simple js resume constructor from json data
+A simple js resume constructor from json data, built for easy and simple modification for resume tailoring
 ![Example](example.png)
+## Modification
+If you want to modify the resume, all you have to do is find the attribute you want to modify from the base resume, then make a modified definition of that attribute in the mod.json file and that will temporarily overrwirte that part of the resume, so if you want to keep a specific version of your resume, all you need to do if keep that mod file.
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+let dat;
+
 window.onload = function () {
-    ["header", "main", "sidebar"].forEach(function (id) {
-        buildHTML(id);
+    fetchAndMergeData().then(() => {
+        console.log(dat);
+        ["header", "main", "sidebar"].forEach(function (id) {
+            buildHTML(id);
+        });
+        applyStyles();
     });
-    applyStyles();
 };
+
+function fetchAndMergeData() {
+    // Fetch data from both JSON files
+    return Promise.all([
+        fetch('data/data.json').then(response => response.json()),
+        fetch('data/mod.json').then(response => response.json()).catch(() => ({}))
+    ])
+    .then(([data, mod]) => {
+        // Merge data - mod data will overwrite data if same field exists
+        dat = {...data, ...mod};
+    });
+}
+
 
 function buildHTML(id) {
     switch (id) {
+        case "header":
+            createHeaderElement();
+            break;
         case "main":
             createExperienceElements();
             break;
@@ -20,7 +41,7 @@ function buildHTML(id) {
                     document.getElementById(id).innerHTML = data;
                 });
             break;
-        }
+    }
 }
 
 function applyStyles() {
@@ -31,117 +52,132 @@ function applyStyles() {
 }
 
 function createExperienceElements() {
-    fetch("data/data.json")
-        .then((response) => response.json())
-        .then((data) => {
-            let experiencesDiv = document.getElementById("main");
-            let h2 = document.createElement("h2");
-            h2.textContent = "Experiences";
-            experiencesDiv.appendChild(h2);
-            data.experiences.forEach((exp) => {
-                let h3 = document.createElement("h3");
-                h3.textContent = exp.title;
-                experiencesDiv.appendChild(h3);
 
-                let p = document.createElement("p");
-                p.textContent = exp.period;
-                experiencesDiv.appendChild(p);
+    let experiencesDiv = document.getElementById("main");
+    let h2 = document.createElement("h2");
+    h2.textContent = "Experiences";
+    experiencesDiv.appendChild(h2);
+    dat.experiences.forEach((exp) => {
+        let h3 = document.createElement("h3");
+        h3.textContent = exp.title;
+        experiencesDiv.appendChild(h3);
 
-                let ul = document.createElement("ul");
-                exp.tasks.forEach((task) => {
-                    let li = document.createElement("li");
-                    li.textContent = task;
-                    ul.appendChild(li);
-                });
-                experiencesDiv.appendChild(ul);
-            });
-        });
-}
-function createSidebarElements() {
-    fetch("data/data.json")
-        .then((response) => response.json())
-        .then((data) => {
-            var sidebarDiv = document.getElementById("sidebar");
+        let p = document.createElement("p");
+        p.textContent = exp.period;
+        experiencesDiv.appendChild(p);
 
-            // Education
-            var h2 = document.createElement("h2");
-            h2.textContent = "Education";
-            sidebarDiv.appendChild(h2);
-
-            data.education.forEach((edu) => {
-                var h3 = document.createElement("h3");
-                h3.textContent = edu.school;
-                sidebarDiv.appendChild(h3);
-                
-                var p = document.createElement("p");
-                p.textContent = edu.period;
-                sidebarDiv.appendChild(p);
-                
-                var p = document.createElement("p");
-                p.textContent = edu.major;
-                sidebarDiv.appendChild(p);
-
-                var ul = document.createElement("ul");
-                edu.notes.forEach((note) => {
-                    var li = document.createElement("li");
-                    li.textContent = note;
-                    ul.appendChild(li);
-                });
-                sidebarDiv.appendChild(ul);
-            });
-
-            // Skills
-            var h2 = document.createElement("h2");
-            h2.textContent = "Skills";
-            sidebarDiv.appendChild(h2);
-
-            var ul = document.createElement("ul");
-            
-            var li = document.createElement("li");
-            li.textContent = "Proficient in " + data.skills[0].languages.join(', ');
+        let ul = document.createElement("ul");
+        exp.tasks.forEach((task) => {
+            let li = document.createElement("li");
+            li.textContent = task;
             ul.appendChild(li);
-
-            data.skills[0].notes.forEach((note) => {
-                if(note == "") return;
-                li = document.createElement("li");
-                li.textContent = note;
-                ul.appendChild(li);
-            });
-            sidebarDiv.appendChild(ul);
-
-
-            // Projects
-            h2 = document.createElement("h2");
-            h2.textContent = "Projects";
-            sidebarDiv.appendChild(h2);
-
-            var ul = document.createElement("ul");
-            data.projects.forEach((proj) => {
-                var li = document.createElement("li");
-                var a = document.createElement("a");
-                a.textContent = proj.name;
-                a.href = proj.link;
-                a.target = "_blank";
-                li.appendChild(a);
-                ul.appendChild(li);
-            });
-            sidebarDiv.appendChild(ul);
-
-            // Info
-            h2 = document.createElement("h2");
-            h2.textContent = "Info";
-            sidebarDiv.appendChild(h2);
-
-            ul = document.createElement("ul");
-            data.Info[0].links.forEach((info) => {
-                li = document.createElement("li");
-                var a = document.createElement("a");
-                a.textContent = info.name;
-                a.href = info.link;
-                a.target = "_blank";
-                li.appendChild(a);
-                ul.appendChild(li);
-            });
-            sidebarDiv.appendChild(ul);
         });
+        experiencesDiv.appendChild(ul);
+    });
+}
+
+function createHeaderElement() {
+    let name = dat.name;
+    let desc = dat.desc;
+
+    // Replace placeholders in description
+    desc = desc.replace('${hook}', dat.hook)
+        .replace('${strength}', dat.strength)
+        .replace('${title}', dat.title)
+        .replace('${position}', dat.position)
+        .replace('${company}', dat.company)
+        .replace('${additionalInfo}', dat.additionalInfo);
+
+    // Create header element
+    let headerHTML = `<h1>${name}</h1><p class="description">${desc}</p>`;
+
+    // Set HTML of header
+    document.getElementById('header').innerHTML = headerHTML;
+}
+
+
+function createSidebarElements() {
+
+    var sidebarDiv = document.getElementById("sidebar");
+
+    // Education
+    var h2 = document.createElement("h2");
+    h2.textContent = "Education";
+    sidebarDiv.appendChild(h2);
+
+    dat.education.forEach((edu) => {
+        var h3 = document.createElement("h3");
+        h3.textContent = edu.school;
+        sidebarDiv.appendChild(h3);
+
+        var p = document.createElement("p");
+        p.textContent = edu.period;
+        sidebarDiv.appendChild(p);
+
+        var p = document.createElement("p");
+        p.textContent = edu.major;
+        sidebarDiv.appendChild(p);
+
+        var ul = document.createElement("ul");
+        edu.notes.forEach((note) => {
+            var li = document.createElement("li");
+            li.textContent = note;
+            ul.appendChild(li);
+        });
+        sidebarDiv.appendChild(ul);
+    });
+
+    // Skills
+    var h2 = document.createElement("h2");
+    h2.textContent = "Skills";
+    sidebarDiv.appendChild(h2);
+
+    var ul = document.createElement("ul");
+
+    var li = document.createElement("li");
+    li.textContent = "Proficient in " + dat.skills[0].languages.join(', ');
+    ul.appendChild(li);
+
+    dat.skills[0].notes.forEach((note) => {
+        if (note == "") return;
+        li = document.createElement("li");
+        li.textContent = note;
+        ul.appendChild(li);
+    });
+    sidebarDiv.appendChild(ul);
+
+
+    // Projects
+    h2 = document.createElement("h2");
+    h2.textContent = "Projects";
+    sidebarDiv.appendChild(h2);
+
+    var ul = document.createElement("ul");
+    dat.projects.forEach((proj) => {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.textContent = proj.name;
+        a.href = proj.link;
+        a.target = "_blank";
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+    sidebarDiv.appendChild(ul);
+
+    // Info
+    h2 = document.createElement("h2");
+    h2.textContent = "Info";
+    sidebarDiv.appendChild(h2);
+
+    ul = document.createElement("ul");
+    dat.Info[0].links.forEach((info) => {
+        li = document.createElement("li");
+        var a = document.createElement("a");
+        a.textContent = info.name;
+        a.href = info.link;
+        a.target = "_blank";
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+    sidebarDiv.appendChild(ul);
 }
